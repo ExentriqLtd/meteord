7aae73c9587d555f81b36f9ea59d154ec21f0498
Tomas Vesely <tomas@vesely.in>
2018-01-05 11:24:13 +0100

    update node

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential g++ python git graphicsmagick pkg-config
+apt-get install -y curl bzip2 build-essential g++ python git graphicsmagick pkg-config libcairo2-dev libjpeg-dev libpango1.0-dev libgif-dev
